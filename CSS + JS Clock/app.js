08c67314080ab0994380ea955fdbd79db91540ae
niczyji <niczyji@gmail.com>
2023-07-28 19:48:51 +0200

    Add proper comms, add defaultOffset variable, add result img

--- CSS + JS Clock/app.js
+++ CSS + JS Clock/app.js
@@ -1,6 +1,7 @@
 function updateClock() {
-  const degPerMinuteOrSecond = 360 / 60; // Jede Sekunde /Minute entspricht 6 Grad
-  const degPerHour = 360 / 12; // Jede Stunde entspricht 30 Grad
+  const degPerMinuteOrSecond = 360 / 60; // Each second/minute corresponds to 6 degrees
+  const degPerHour = 360 / 12; // Each hour corresponds to 30 degrees
+  const defaultOffset = 90; // Offset to correctly position clock hands
 
   function normalizeDegrees(degrees) {
     if (degrees >= 360) {
@@ -16,14 +17,14 @@ function updateClock() {
     // Update the second hand
     const seconds = now.getSeconds();
     const secondHand = document.querySelector(".second-hand");
-    let setSecond = seconds * degPerMinuteOrSecond + 90; // Start position calculation
+    let setSecond = seconds * degPerMinuteOrSecond + defaultOffset; // Start position calculation
     setSecond = normalizeDegrees(setSecond);
     secondHand.style.transform = `rotate(${setSecond}deg)`; // Rotate the hand
 
     // Update the minute hand
     let minutes = now.getMinutes();
     const minuteHand = document.querySelector(".min-hand");
-    let setMinute = minutes * degPerMinuteOrSecond + 90; // Start position calculation
+    let setMinute = minutes * degPerMinuteOrSecond + defaultOffset; // Start position calculation
     setMinute = normalizeDegrees(setMinute);
     minuteHand.style.transform = `rotate(${setMinute}deg)`; // Rotate the hand
 
@@ -31,7 +32,7 @@ function updateClock() {
     let hours = now.getHours();
     hours = hours % 12;
     const hourHand = document.querySelector(".hour-hand");
-    let setHour = hours * degPerHour + 90; // Start position calculation
+    let setHour = hours * degPerHour + defaultOffset; // Start position calculation
     setHour = normalizeDegrees(setHour);
     hourHand.style.transform = `rotate(${setHour}deg)`; // Rotate the hand
   }, 1000);
