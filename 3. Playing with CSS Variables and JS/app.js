337cedb1b87507cbe3df979d730bcc2836e2236d
niczyji <niczyji@gmail.com>
2023-07-29 12:26:43 +0200

    Feature: Add Project Playing with CSS Variables and JS

---
+++ 3. Playing with CSS Variables and JS/app.js
@@ -0,0 +1,16 @@
+// Get elements
+let spacingInput = document.querySelector('input[name="spacing"]');
+let blurInput = document.querySelector('input[name="blur"]');
+let baseColorInput = document.querySelector('input[name="base"]');
+let root = document.documentElement;
+
+document.addEventListener("change", function (e) {
+  let spacingValue = spacingInput.value + "px"; // Append 'px' to the value
+  root.style.setProperty("--spacing", spacingValue);
+
+  let blurValue = blurInput.value + "px"; // Append 'px' to the value
+  root.style.setProperty("--blur", blurValue);
+
+  let baseValue = baseColorInput.value;
+  root.style.setProperty("--base", baseValue);
+});
