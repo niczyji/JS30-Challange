9b8193f9d1ffe00c1a308a3821f8a5956d2881a8
niczyji <niczyji@gmail.com>
2023-08-04 19:59:03 +0200

    Feature: Add function handleCheck to handle checkbox click and selection

--- 9. Hold Shift to Check Multiple Checkboxes/app.js
+++ 9. Hold Shift to Check Multiple Checkboxes/app.js
@@ -1,5 +1,33 @@
-setTimeout(function () {
-  document
-    .querySelector('input[type="checkbox"]')
-    .setAttribute("checked", true);
-}, 100);
+// Select all input elements with the class "item" and type "checkbox"
+const items = document.querySelectorAll(".item input[type='checkbox']");
+
+// Variable to store the last checked checkbox element
+let lastChecked;
+
+// Function to handle checkbox click and selection
+function handleCheck(e) {
+  let inBetween = false; // Flag variable to determine if we are in between two selected elements
+
+  // If the shift key is pressed and this checkbox is checked
+  if (e.shiftKey && this.checked) {
+    // Loop through all the checkboxes
+    items.forEach((checkbox) => {
+      // If the current checkbox is the same as the current checkbox being clicked or the last checked checkbox
+      if (checkbox === this || checkbox === lastChecked) {
+        // Toggle the inBetween flag to determine if we are in between the two selected elements
+        inBetween = !inBetween;
+      }
+      // If we are in between two selected elements
+      if (inBetween) {
+        // Set the checked property of the checkbox to true to select it
+        checkbox.checked = true;
+      }
+    });
+  }
+
+  // Update lastChecked to store the currently clicked checkbox
+  lastChecked = this;
+}
+
+// Add a click event listener to each checkbox element to handle the click behavior
+items.forEach((item) => item.addEventListener("click", handleCheck));
