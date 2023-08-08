7b0141f1a47454c33d8ee2481946f382fd385c0c
niczyji <niczyji@gmail.com>
2023-08-08 12:32:21 +0200

    feat: enhance video player controls

--- 10. Custom HTML5 Video Player/app.js
+++ 10. Custom HTML5 Video Player/app.js
@@ -1,17 +1,49 @@
-const player = document.querySelector(".player");
-const video = document.querySelector("video");
+const videoPlayer = document.querySelector(".player");
+const videoContent = videoPlayer.querySelector("video");
 
-const progressBar = document.getElementById("progress-bar");
-const toggle = document.getElementById("toggle-video");
-const toggleMute = document.getElementById("toggle-mute");
-const volumeSlider = document.getElementById("volume-slider");
-const sliders = document.querySelectorAll(".player-slider");
-const skipButtons = document.querySelectorAll(".playback-btn");
-const fullscreen = document.getElementById("fullscreen");
+const progressControl = document.getElementById("progress-bar");
+const playPauseButton = document.getElementById("toggle-video");
+const muteUnmuteButton = document.getElementById("toggle-mute");
+const volumeControl = document.getElementById("volume-slider");
+const controlSliders = videoPlayer.querySelectorAll(".player-slider");
+const playbackControls = videoPlayer.querySelectorAll(".playback-btn");
+const fullscreenButton = document.getElementById("fullscreen");
 
-toggle.addEventListener("click", toggleVideo);
-video.addEventListener("click", toggleVideo);
+playPauseButton.addEventListener("click", handlePlayPause);
+playPauseButton.addEventListener("click", togglePlayPauseIcon);
+videoContent.addEventListener("click", handlePlayPause);
+muteUnmuteButton.addEventListener("click", videoVolumeToggle);
+muteUnmuteButton.addEventListener("click", toggleMuteIcon);
 
-function toggleVideo() {
-  video.paused ? video.play() : video.pause();
+function handlePlayPause() {
+  // Toggle Video Playback
+  videoContent.paused ? videoContent.play() : videoContent.pause();
+}
+
+function togglePlayPauseIcon() {
+  // Toggle Icon
+  const icon = playPauseButton.children[0];
+  if (icon.classList.contains("fa-play")) {
+    icon.classList.remove("fa-play");
+    icon.classList.add("fa-pause");
+  } else if (icon.classList.contains("fa-pause")) {
+    icon.classList.remove("fa-pause");
+    icon.classList.add("fa-play");
+  }
+}
+
+function videoVolumeToggle() {
+  videoContent.volume = videoContent.volume === 0 ? 1 : 0;
+}
+
+function toggleMuteIcon() {
+  // Toggle Icon
+  const icon = muteUnmuteButton.children[0];
+  if (icon.classList.contains("fa-volume-up")) {
+    icon.classList.remove("fa-volume-up");
+    icon.classList.add("fa-volume-off");
+  } else if (icon.classList.contains("fa-volume-off")) {
+    icon.classList.remove("fa-volume-off");
+    icon.classList.add("fa-volume-up");
+  }
 }
