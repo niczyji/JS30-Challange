f62b6841a6c1f4c7007f6df22876eef7cd16d5f7
niczyji <niczyji@gmail.com>
2023-08-11 07:33:10 +0200

    Feature: add js code detector

--- 11. Key Sequence Detection/app.js
+++ 11. Key Sequence Detection/app.js
@@ -1,8 +1,12 @@
 const pressed = [];
-const secredCode = "k0d3";
+const secredCode = "code";
 
 window.addEventListener('keyup', (e)=> {
-  console.log(e.key);
   pressed.push(e.key);
+  pressed.splice(-secredCode.length - 1, pressed.length - secredCode.length)
+  if (pressed.join('').includes(secredCode)) {
+    console.log("match");
+    cornify_add();
+  }
   console.log(pressed);
 })
