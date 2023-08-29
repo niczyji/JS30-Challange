b0d2f5e7de92eb8d76e296df16ad983776ecee2a
niczyji <niczyji@gmail.com>
2023-08-29 15:05:02 +0200

    Add Object References / Copy

--- 13. JS References vs Copying/app.js
+++ 13. JS References vs Copying/app.js
@@ -4,11 +4,11 @@
 const players = ["Wes", "Sarah", "Ryan", "Poppy"];
 
 // and we want to make a copy of it.
-
 const team = players;
 
 // You might think we can just do something like this:
 team[3] = "Mika";
+
 //  however what happens when we update that array?
 
 // now here is the problem!
@@ -18,12 +18,16 @@ team[3] = "Mika";
 // Why? It's because that is an array reference, not an array copy. They both point to the same array!
 
 // So, how do we fix this? We take a copy instead!
-
-// one way
+const team2 = players.slice();
+// one day
 
 // or create a new array and concat the old one in
+const team3 = [].concat(players);
 
 // or use the new ES6 Spread
+const team4 = [...players];
+
+const team5 = Array.from(players);
 
 // now when we update it, the original one isn't changed
 
@@ -36,9 +40,26 @@ const person = {
 };
 
 // and think we make a copy:
+const captain = person;
+captain.number = 99;
 
 // how do we take a copy instead?
+const cap2 = Object.assign({}, person, { number: 99 });
 
 // We will hopefully soon see the object ...spread
+const cap3 = { ...person };
 
 // Things to note - this is only 1 level deep - both for Arrays and Objects. lodash has a cloneDeep method, but you should think twice before using it.
+
+const eMusk = {
+  name: "Elon Musk",
+  age: 96,
+  social: {
+    x: "X",
+    facebook: "X",
+  },
+};
+
+const dev = Object.assign({}, wes);
+
+const dev2 = JSON.parse(JSON.stringify(eMusk));
