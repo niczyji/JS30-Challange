3dcf92e0782bc87ff89da2617f214f68bbf1b87b
niczyji <niczyji@gmail.com>
2023-09-08 09:16:44 +0200

    Add Event Delegation

--- 14. LocalStorage and Event Delegation/app.js
+++ 14. LocalStorage and Event Delegation/app.js
@@ -31,6 +31,16 @@ function populateList(plates = [], platesList) {
     .join("");
 }
 
+function toggleDone(e) {
+  if (!e.target.matches("input")) return;
+  const el = e.target;
+  const index = el.dataset.index;
+  items[index].done = !items[index].done;
+  localStorage.setItems("items", JSON.stringify(items));
+  populateList(items, itemsList);
+}
+
 addItems.addEventListener("submit", addItem);
+itemsList.addEventListener("click", toggleDone);
 
 populateList(items, itemsList);
