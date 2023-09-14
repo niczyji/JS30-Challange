d13a7a7c9e2392c3196ffc9d87c6f4a5df3fd1d7
niczyji <niczyji@gmail.com>
2023-09-14 10:49:09 +0200

    Add Comments

--- 17. Adding Up Times with Reduce/app.js
+++ 17. Adding Up Times with Reduce/app.js
@@ -1,19 +1,22 @@
 const videoListItems = document.querySelectorAll(".videos li");
 
+// Create an array from the 'data-time' attributes
 const videoTimes = Array.from(videoListItems).map((item) =>
   item.getAttribute("data-time")
 );
 
+// Calculate the total time in seconds
 const totalTime = videoTimes.reduce((acc, time) => {
   const [minutes, seconds] = time.split(":").map(Number);
   return acc + (minutes * 60 + seconds);
 }, 0);
 
+// Convert the total time into hours, minutes, and seconds
 const totalHours = Math.floor(totalTime / 3600);
 const remainingSecondsForMinutes = totalTime % 3600;
 const totalMinutes = Math.floor(remainingSecondsForMinutes / 60);
 const totalSeconds = remainingSecondsForMinutes % 60;
 
 console.log(
-  `${totalTime} Die Gesamtzeit beträgt ${totalHours} Stunden, ${totalMinutes} Minuten und ${totalSeconds} Sekunden.`
+  `Die Gesamtzeit beträgt ${totalHours} Stunden, ${totalMinutes} Minuten und ${totalSeconds} Sekunden.`
 );
